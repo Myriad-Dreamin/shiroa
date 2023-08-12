e399d2b1a97cefac5f50b75213127100c0c123ef
Myriad-Dreamin <camiyoru@gmail.com>
2023-08-13 00:46:49 +0800

    dev(cli): init serve command

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -1,3 +1,43 @@
 pub mod compile;
+pub mod render;
 pub mod serve;
 pub mod summary;
+
+use std::path::PathBuf;
+
+use clap::{ArgAction, Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[clap(name = "typst-book", version = "0.1.0")]
+pub struct Opts {
+    #[clap(subcommand)]
+    pub sub: Option<Subcommands>,
+}
+
+#[derive(Debug, Subcommand)]
+#[clap(
+    about = "The cli for typst.ts.",
+    after_help = "",
+    next_display_order = None
+)]
+#[allow(clippy::large_enum_variant)]
+pub enum Subcommands {
+    #[clap(about = "serve book.")]
+    Serve(ServeArgs),
+}
+
+#[derive(Default, Debug, Clone, Parser)]
+#[clap(next_help_heading = "Compile options")]
+pub struct ServeArgs {
+    /// Path to typst workspace.
+    #[clap(long, short, default_value = ".")]
+    pub workspace: String,
+
+    /// Output to directory, default in the same directory as the entry file.
+    #[clap(long, short, default_value = "")]
+    pub output: String,
+
+    /// Add additional directories to search for fonts
+    #[clap(long = "font-path", value_name = "DIR", action = ArgAction::Append)]
+    pub font_paths: Vec<PathBuf>,
+}
--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,40 @@
+use std::process::exit;
+
+use clap::{Args, Command, FromArgMatches};
+use typst_book_cli::{Opts, ServeArgs, Subcommands};
+
+fn get_cli(sub_command_required: bool) -> Command {
+    let cli = Command::new("$").disable_version_flag(true);
+    Opts::augment_args(cli).subcommand_required(sub_command_required)
+}
+
+fn help_sub_command() {
+    Opts::from_arg_matches(&get_cli(true).get_matches()).unwrap();
+}
+
 fn main() {
-    println!("hello world")
+    let opts = Opts::from_arg_matches(&get_cli(false).get_matches())
+        .map_err(|err| err.exit())
+        .unwrap();
+
+    env_logger::builder()
+        .filter_level(log::LevelFilter::Info)
+        .filter_module("typst::", log::LevelFilter::Warn)
+        .filter_module("typst_library::", log::LevelFilter::Warn)
+        .init();
+
+    match opts.sub {
+        Some(Subcommands::Serve(args)) => serve(args),
+        None => help_sub_command(),
+    };
+
+    #[allow(unreachable_code)]
+    {
+        unreachable!("The subcommand must exit the process.");
+    }
+}
+
+fn serve(_args: ServeArgs) -> ! {
+    println!("hello world");
+    exit(0)
 }
