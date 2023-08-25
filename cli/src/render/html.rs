84d9cf9f82476569c8976b688c8e24a2d55011b5
Myriad-Dreamin <camiyoru@gmail.com>
2023-08-25 23:50:30 +0800

    fix(cli): correct preferred_dark_theme

--- cli/src/render/html.rs
+++ cli/src/render/html.rs
@@ -44,7 +44,7 @@ impl HtmlRenderer {
 
         data.insert("fold_enable".to_owned(), json!(false));
         data.insert("fold_level".to_owned(), json!(0u64));
-        data.insert("preferred_dark_theme".to_owned(), json!("dark"));
+        data.insert("preferred_dark_theme".to_owned(), json!("ayu"));
         data.insert("default_theme".to_owned(), json!("light"));
         data.insert("book_title".to_owned(), data["title"].clone());
         if let Some(repo) = data.get("repository") {
