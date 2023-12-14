d7eab59d8926fae705503c7950e73269fe4c6b74
Myriad-Dreamin <camiyoru@gmail.com>
2023-12-14 09:19:44 +0800

    build: update font list

--- cli/src/font.rs
+++ cli/src/font.rs
@@ -23,6 +23,24 @@ pub static EMBEDDED_FONT: &[Cow<'_, [u8]>] = &[
     Cow::Borrowed(font!("DejaVuSansMono-Bold.ttf")),
     Cow::Borrowed(font!("DejaVuSansMono-Oblique.ttf")),
     Cow::Borrowed(font!("DejaVuSansMono-BoldOblique.ttf")),
+    // Embed CJK fonts.
+    #[cfg(feature = "embedded-cjk-fonts")]
+    Cow::Borrowed(font!("InriaSerif-Bold.ttf")),
+    #[cfg(feature = "embedded-cjk-fonts")]
+    Cow::Borrowed(font!("InriaSerif-BoldItalic.ttf")),
+    #[cfg(feature = "embedded-cjk-fonts")]
+    Cow::Borrowed(font!("InriaSerif-Italic.ttf")),
+    #[cfg(feature = "embedded-cjk-fonts")]
+    Cow::Borrowed(font!("InriaSerif-Regular.ttf")),
+    #[cfg(feature = "embedded-cjk-fonts")]
+    Cow::Borrowed(font!("Roboto-Regular.ttf")),
+    #[cfg(feature = "embedded-cjk-fonts")]
+    Cow::Borrowed(font!("NotoSerifCJKsc-Regular.otf")),
+    // Embed emoji fonts.
+    #[cfg(feature = "embedded-emoji-fonts")]
+    Cow::Borrowed(font!("TwitterColorEmoji.ttf")),
+    #[cfg(feature = "embedded-emoji-fonts")]
+    Cow::Borrowed(font!("NotoColorEmoji.ttf")),
 ];
 
 #[cfg(not(feature = "embedded-fonts"))]
